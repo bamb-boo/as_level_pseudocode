01416b66b058abcddd61aae25535089e637c7178
bamb-boo <gsbarathraam18@gmail.com>
2026-08-20 11:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         runn = sys.argv[1] # if you're running via 'python main.py marks.pseudo'
     else:
-        runn = "marks.pseudo" # give the file path if you're using run via main.py
+        runn = "examples/example1.pseudo" # give the file path if you're using run via main.py
     run(runn)
